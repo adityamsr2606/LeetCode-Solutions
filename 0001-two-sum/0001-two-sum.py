59285440c24d80b585e118a89eccefa4c6b70516
Aditya Mohan Srivastava <157827784+adityamsr2606@users.noreply.github.com>
2026-08-03 01:49:29 +0530

    Time: 1745 ms (16.81%), Space: 19.7 MB (94.8%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,17 +1,8 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        seen = {}  # number -> index
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n-1):
+            for j in range(i+1,n):
+                if nums[i]+nums[j]==target:
+                    return[i,j]
         
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            
-            if complement in seen:
-                return [seen[complement], i]
-            
-            seen[nums[i]] = i
-            
